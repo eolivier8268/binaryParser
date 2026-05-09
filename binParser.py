0e951ce547cf2d50ef789dc5d4407e9446b24f56
Emile Olivier <c26emile.olivier@afacademy.af.edu>
2026-05-08 21:51:02 -0600

    switch to factory method for PE class, remove old Win32 class

--- binParser.py
+++ binParser.py
@@ -155,95 +155,6 @@ class Elf(File):
         else:                               return True
 
 
-# Sourced from - https://0xrick.github.io/categories/#win-internals
-class Win32(File):
-    def __init__(self, filename):
-        super().__init__(filename)
-        self.filetype = "PE32"
-        # TODO: convert individual headers to classes
-        self.IMAGE_DOS_HEADER = self.bytes[0:0x40]
-        self.parse_dos_header(self.IMAGE_DOS_HEADER)
-        self.IMAGE_DOS_STUB = self.bytes[0x40:0x80]
-        if (self.e_lfanew > 0x80):
-            self.IMAGE_RICH_HEADER = self.bytes[0x80:self.e_lfanew]
-        else:
-            self.IMAGE_RICH_HEADER = None
-        self.IMAGE_NT_HEADERS = self.bytes[self.e_lfanew:self.e_lfanew+100]
-        self.parse_nt_headers()
-        self.filetype = self.getStructure()
-    
-    def isValidPE(self):
-        if self.e_magic != b'MZ':               return False
-        elif self.Signature != b'PE\x00\x00':   return False
-        elif    ((self.Magic != b'\x0b\x01') and
-                (self.Magic != b'\x0b\x02') and 
-                (self.Magic != b'\x07\x01')):   return False
-        else:                                   return True
-
-    def getStructure(self):
-        return
-
-    def parse_dos_header(self, IMAGE_DOS_HEADER):
-        self.e_magic = IMAGE_DOS_HEADER[0:2]            # Magic number
-        self.e_cblp = IMAGE_DOS_HEADER[2:4]             # Bytes on last page of file
-        self.e_cp = IMAGE_DOS_HEADER[4:6]               # Pages in file
-        self.e_crlc = IMAGE_DOS_HEADER[6:8]             # Relocations
-        self.e_cparhdr = IMAGE_DOS_HEADER[8:0xa]        # Size of header in paragraphs
-        self.e_minalloc = IMAGE_DOS_HEADER[0xa:0xc]     # Minimum extra paragraphs needed
-        self.e_maxalloc = IMAGE_DOS_HEADER[0xc:0xe]     # Maximum extra paragraphs needed
-        self.e_ss = IMAGE_DOS_HEADER[0xe:0x10]          # Initial (relative) SS value
-        self.e_sp = IMAGE_DOS_HEADER[0x10:0x12]         # Initial SP value
-        self.e_csum = IMAGE_DOS_HEADER[0x12:0x14]       # Checksum
-        self.e_ip = IMAGE_DOS_HEADER[0x14:0x16]         # Initial IP value
-        self.e_cs = IMAGE_DOS_HEADER[0x16:0x18]         # Initial (relative) CS value
-        self.e_lfarlc = IMAGE_DOS_HEADER[0x18:0x1a]     # File address of relocation table
-        self.e_ovno = IMAGE_DOS_HEADER[0x1a:0x1c]       # Overlay number
-        self.e_res = IMAGE_DOS_HEADER[0x1c:0x24]        # Reserved words
-        self.e_oemid = IMAGE_DOS_HEADER[0x24:0x26]      # OEM identifier (for e_oeminfo)
-        self.e_oeminfo = IMAGE_DOS_HEADER[0x26:0x28]    # OEM information = IMAGE_DOS_HEADER[] # e_oemid specific
-        self.e_res2 = IMAGE_DOS_HEADER[0x28:0x3c]       # Reserved words
-        self.e_lfanew = IMAGE_DOS_HEADER[0x3c:0x40]     # File address of new exe header
-        self.e_lfanew = int.from_bytes(self.e_lfanew, byteorder='little')
-    
-    def parse_rich_header(self):
-        return
-    
-    def parse_nt_headers(self):
-        self.Signature = self.IMAGE_NT_HEADERS[0:4]
-        self.FileHeader = self.IMAGE_NT_HEADERS[4:0x18]
-        self.parse_coff_header(self.FileHeader)
-        # TODO: pull sizeOfOH from COFF Header
-        self.OptionalHeader = self.IMAGE_NT_HEADERS[0x18:0x18+self.SizeOfOptionalHeader]
-        self.parse_optional_header(self.OptionalHeader)
-
-    def parse_coff_header(self, header):
-        # TODO: go through MS docs and get actual values for each
-        # byte representation of Machine
-        self.Machine =                  header[0x00:0x02]
-        self.NumberOfSections =         header[0x02:0x04]
-        # TODO: convert to actual timestamp
-        self.TimeDateStamp =            header[0x04:0x08]
-        self.PointerToSymbolTable =     header[0x08:0x0c]
-        self.NumberOfSymbols =          header[0x0c:0x10]
-        sizeOfOptionalHeader =          header[0x10:0x12]
-        self.SizeOfOptionalHeader =     int.from_bytes(sizeOfOptionalHeader, byteorder='little')
-        # TODO: go through MS docs and get actual values for each
-        # byte representation of Characteristics
-        self.Characteristics =          header[0x12:0x14]
-
-    def parse_optional_header(self, header):
-        self.Magic = header[0x00:0x02]
-        self.MajorLinkerVersion = header[0x02:0x03]
-        self.MinorLinkerVersion = header[0x03:0x04]
-        self.SizeOfCode = header[0x04:0x08]
-        self.SizeOfInitializedData = header[0x08:0x0c]
-        self.SizeOfUninitializedData = header[0xc:0x10]
-        self.AddressOfEntryPoint = header[0x10:0x14]
-        self.BaseOfCode = header[0x14:0x18]
-        self.BaseOfData = header[0x18:0x1c]
-        # TODO: add additional/optional fields
-        return
-
 # DOS header - same across 32-bit and 64-bit arch
 class IMAGE_DOS_HEADER(ByteStream):
     def __init__(self, rawBytes):
@@ -395,29 +306,47 @@ class IMAGE_OPTIONAL_HEADER(ByteStream):
 
 # TODO: Create unified PE class that Win32 and Win64 can inherit from
 class PE(File):
-    def __init__(self, filename):
-        super().__init__(filename)
-        self.filetype = "PE"
-        # TODO: convert individual headers to classes
-        self.dosHeader = IMAGE_DOS_HEADER(self.bytes[0:0x40])
-        self.dosStub = IMAGE_DOS_STUB(self.bytes[0x40:0x80])
-        if (self.dosHeader.e_lfanew > 0x80):
-            self.richHeader = IMAGE_RICH_HEADER(self.bytes[0x80:self.dosHeader.e_lfanew])
-        else:
-            self.richHeader = None
-        self.ntHeaders = IMAGE_NT_HEADERS(self.bytes[self.dosHeader.e_lfanew:self.dosHeader.e_lfanew+100])
-        self.filetype = self.getStructure()
+    def __init__(self, filename, bytes, dosHeader, dosStub, richHeader, ntHeaders, coffHeader, optionalHeader):
+        self.filename = filename
+        self.bytes = bytes
+        self.dosHeader = dosHeader
+        self.dosStub = dosStub
+        self.richHeader = richHeader
+        self.ntHeaders = ntHeaders
+        self.coffHeader = coffHeader
+        self.optionalHeader = optionalHeader
     
-    def isValidPE(self):
+    @staticmethod
+    def read_bytes(filename):
+        rawBytes = b''
+        with open(filename, "rb") as f:
+            rawBytes = f.read()
+        return rawBytes
+    
+    @classmethod
+    def from_file(cls, filename):
+        rawBytes = cls.read_bytes(filename)
+        dosHeader = IMAGE_DOS_HEADER(rawBytes[0:0x40])
+        dosStub = IMAGE_DOS_STUB(rawBytes[0x40:0x80])
+        if (dosHeader.e_lfanew > 0x80):
+            richHeader = IMAGE_RICH_HEADER(rawBytes[0x80:dosHeader.e_lfanew])
+        else:
+            richHeader = None
+        ntHeaders = IMAGE_NT_HEADERS(rawBytes[dosHeader.e_lfanew:dosHeader.e_lfanew+100])
+
+        return cls(filename, rawBytes, dosHeader, dosStub, richHeader, ntHeaders, ntHeaders.coffHeader, ntHeaders.OptionalHeader)
+
+    def is_valid_PE(self):
         if self.dosHeader.e_magic != b'MZ':               return False
         elif self.ntHeaders.Signature != b'PE\x00\x00':   return False
-        elif    ((self.ntHeaders.OptionalHeader.Magic != b'\x0b\x01') and
-                (self.ntHeaders.OptionalHeader.Magic != b'\x0b\x02') and 
-                (self.ntHeaders.OptionalHeader.Magic != b'\x07\x01')):   return False
+        elif    ((self.optionalHeader.Magic != b'\x0b\x01') and
+                (self.optionalHeader.Magic != b'\x0b\x02') and 
+                (self.optionalHeader.Magic != b'\x07\x01')):   return False
         else:                                   return True
 
-    def getStructure(self):
-        return
+    @property
+    def structure(self):
+        return None
         
 
 def main():
@@ -439,17 +368,17 @@ def main():
     print(atm.structure)
     print("======================================")
 
-    # exe1 = PE("./samples/pe32.exe")
-    # print(exe1)
-    # print(exe1.dosStub.message)
-    # print(exe1.isValidPE())
-    # print(exe1.ntHeaders.Signature)
-    # # print(exe1.Magic)
-    # print(exe1.dosHeader)
-    # print(exe1.dosStub)
-    # print(exe1.richHeader)
-    # print(exe1.ntHeaders)
-    # print("======================================")
+    exe1 = PE.from_file("./samples/pe32.exe")
+    print(exe1)
+    print(exe1.dosStub.message)
+    print(exe1.is_valid_PE())
+    print(exe1.ntHeaders.Signature)
+    print(exe1.optionalHeader.Magic)
+    print(exe1.dosHeader)
+    print(exe1.dosStub)
+    print(exe1.richHeader)
+    print(exe1.ntHeaders)
+    print("======================================")
     
     # exe2 = PE("./samples/selenium-manager.exe")
     # print(exe2)
